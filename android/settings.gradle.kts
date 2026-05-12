d208be032dd4ef452d2b229f172fc7b25a5647f5
sansuhaib8-hub <sansuhaib8@gmail.com>
2026-05-12 18:55:32 +0300

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -26,16 +26,14 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-
     id("com.android.application") version "8.7.0" apply false
-
     id("org.jetbrains.kotlin.android") version "1.8.22" apply false
-
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+
     repositories {
         google()
         mavenCentral()
